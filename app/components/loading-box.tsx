c53d9549a1725606a9f740ec433fa8db356b8d7a
lihong.chris <745594100@163.com>
2024-05-15 18:39:22 +0800

    chore: update codes for suspense

--- app/components/loading-box.tsx
+++ app/components/loading-box.tsx
@@ -7,21 +7,27 @@ interface MessageContainerProps {
 }
 
 async function asyncRequest() {
-  return new Promise((resolve) => setTimeout(resolve, 15000, "Hello World"));
+  return new Promise((resolve) => setTimeout(resolve, 1500, "Hello World"));
+}
+
+function Message({ promise }: MessageContainerProps) {
+  const promiseContent = use(promise);
+
+  return (
+    <div className="w-32 h-32 border-4 fade-in text-center fade-in rounded-full flex items-center justify-center">
+      <span>{promiseContent}</span>
+    </div>
+  );
 }
 
 function MessageContainer({ promise }: MessageContainerProps) {
-  const promiseContent = use(promise);
-
-  console.log("promiseContent", promiseContent);
-
-
-  // Something was wrong in suspense
   return (
-    <Suspense fallback={<div className="w-32 h-32 border-4 fade-in text-center">Loading...</div>}>
-      <div className="w-32 h-32 border-4 fade-in text-center fade-in rounded-full flex items-center justify-center">
-        <span>Result: {promiseContent}</span>
-      </div>
+    <Suspense
+      fallback={
+        <div className="w-32 h-32 border-4 text-center flex items-center justify-center"><span>Loading...</span></div>
+      }
+    >
+      <Message promise={promise} />
     </Suspense>
   );
 }
